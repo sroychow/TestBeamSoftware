e29f1315265a16700fc1253aeee777e4f18086cc
Alessandro Rossi <alessandro.rossi@cern.ch>
2016-12-01 15:25:16 +0100

    PG analysis

---
+++ src/basePGReco.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include "TROOT.h"
+#include "TStopwatch.h"
+#include "BasePGAnalysis.h"
+#include "argvparser.h"
+using std::cout;
+using std::cerr;
+using std::endl;
+
+using namespace CommandLineProcessing;
+
+int main( int argc,char* argv[] ){
+  if(argc<2)  {
+    std::cout << "Jobcard missing.\n./baselinReco <jobcardname>" << std::endl;
+    return 1;
+  }
+  std::string jobfile = argv[1];
+  //Let's roll
+  TStopwatch timer;
+  timer.Start();
+  BasePGAnalysis r;
+  r.readJob(jobfile);
+  r.beginJob();
+  std::cout << "Event Loop start" << std::endl;
+  r.eventLoop();
+  r.endJob();
+  timer.Stop();
+  cout << "Realtime/CpuTime = " << timer.RealTime() << "/" << timer.CpuTime() << endl;
+  return 0;
+}
